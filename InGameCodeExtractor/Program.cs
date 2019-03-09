9d7ea544656d2e916a630c02bdc4481be7f699c1
Neyna <3091083+neyna@users.noreply.github.com>
2019-03-09 17:12:47 +0100

    add scipt to auto copy paste the in game code to paste in space engineers

---
+++ InGameCodeExtractor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            string startString = "#region in-game";
+            string line;
+            string trimmed = "";
+            bool read = false;
+            int depth = 0;
+            System.IO.StreamReader file;
+
+
+
+            file = new System.IO.StreamReader("D:\\Dev\\space-engineers-scripts\\SpaceEngineers\\Extractor.cs"); //Open the currently active file
+
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (!read && line.Contains(startString)) read = true;
+                else if (read && line.Contains("#region")) depth++;
+                else if (read && line.Contains("#endregion"))
+                {
+                    if (depth <= 0) break;
+                    else depth--;
+                }
+                else if (read) trimmed += line.Trim() + "\n";
+            }
+            file.Close();
+
+            //Clipboard.SetText(trimmed);
+            if (trimmed!=null && trimmed != "")
+            {
+                Clipboard.SetText(trimmed);
+            }            
+        }
+    }
+}
